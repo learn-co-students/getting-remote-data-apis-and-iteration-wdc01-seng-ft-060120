e567540ac5f8403c1d0a26af50f492281ae1565d
devdave0 <dbchung24@gmail.com>
2020-06-15 11:59:20 -0400

    updated

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -8,20 +8,24 @@ def get_character_movies_from_api(character_name)
 
   response_string = RestClient.get('http://swapi.dev/api/people')
   response_hash = JSON.parse(response_string)
+  film_urls = []  
+  response_hash["results"].each do |result|
+    if result['name'] == character_name
+      film_urls = result['films']
+    end
+  end
+  films = film_urls.map do |url|
+    film_string = RestClient.get(url)
+    film_hash = JSON.parse(film_string)
+  end
 
-  # iterate over the response hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `print_movies`
-  #  and that method will do some nice presentation stuff like puts out a list
-  #  of movies by title. Have a play around with the puts with other info about a given film.
+  
 end
 
 def print_movies(films)
-  # some iteration magic and puts out the movies in a nice list
+  films.each_with_index do |film, index|
+    puts "#{index+1}. #{film["title"]}"
+  end
 end
 
 def show_character_movies(character)
@@ -29,7 +33,4 @@ def show_character_movies(character)
   print_movies(films)
 end
 
-## BONUS
 
-# that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
-# can you split it up into helper methods?
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,9 @@
 def welcome
+  puts "Welcome to the Star Wars API lab"
   # puts out a welcome message here!
 end
 
 def get_character_from_user
   puts "please enter a character name"
-  # use gets to capture the user's input. This method should return that input, downcased.
+  user_input = gets.chomp
 end
